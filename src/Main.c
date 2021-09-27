94cc476cd9949432d18844151786c1828e9ad52d
Tomasz Gardocki <tomasz.gardocki.stud@pw.edu.pl>
2021-09-27 22:44:44 +0200

    Slight code cleanup

--- src/Main.c
+++ src/Main.c
@@ -10,60 +10,15 @@
 ModuleManager moduleManager;
 DisplayManager displayManager;
 
-dictionary* config;
-
-void onExit(int signal) {
-	destroyModuleManager(&moduleManager);
-	destroyDisplayManager(&displayManager);
-	stashDestroy();
-
-#ifdef REQUIRED_CURL
-	curl_global_cleanup();
-#endif
-
-#ifdef REQUIRED_LIBXML
-	xmlCleanupParser();
-#endif
-}
-
 bool loadConfig() {
+	dictionary* config;
 	char* configDirectory = getConfigDirectory();
 	config = iniparser_load(configDirectory);
 	free(configDirectory);
-	return config != NULL;
-}
 
-void unloadConfig() {
-	iniparser_freedict(config);
-}
-
-int main() {
-	struct sigaction signalMgmt;
-	signalMgmt.sa_handler = onExit;
-	sigemptyset(&signalMgmt.sa_mask);
-	signalMgmt.sa_flags = 0;
-	sigaction(SIGINT, &signalMgmt, NULL); // error is not critical, we can safely ignore it
-
-	initModuleManager(&moduleManager);
-	initDisplayManager(&displayManager);
-
-	if(!stashInit()) {
-		fprintf(stderr, "Error loading stash file!\n");
-		return 1;
+	if(config == NULL) {
+		return false;
 	}
-
-	if(!loadConfig()) {
-		fprintf(stderr, "Error loading config file!\n");
-		return 1;
-	}
-
-#ifdef REQUIRED_CURL
-	curl_global_init(CURL_GLOBAL_DEFAULT);
-#endif
-
-#ifdef REQUIRED_LIBXML
-	LIBXML_TEST_VERSION;
-#endif
 
 	int configSectionCount = iniparser_getnsec(config);
 	int keyCount;
@@ -142,8 +97,70 @@ int main() {
 		free(keys);
 	}
 
-	unloadConfig();
+	iniparser_freedict(config);
 	free(globalConfigKeys);
+	return true;
+}
+
+bool initFunctionality() {
+	if(!initModuleManager(&moduleManager)) {
+		fprintf(stderr, "Error initializing module manager!\n");
+		return false;
+	}
+
+	if(!initDisplayManager(&displayManager)) {
+		fprintf(stderr, "Error initializing display manager!\n");
+		return false;
+	}
+
+	if(!stashInit()) {
+		fprintf(stderr, "Error loading stash file!\n");
+		return false;
+	}
+
+#ifdef REQUIRED_CURL
+	if(!curl_global_init(CURL_GLOBAL_DEFAULT)) {
+		fprintf(stderr, "Error initializing CURL library!\n");
+		return false;
+	}
+#endif
+
+#ifdef REQUIRED_LIBXML
+	LIBXML_TEST_VERSION;
+#endif
+
+	if(!loadConfig()) {
+		fprintf(stderr, "Error loading config file!\n");
+		return false;
+	}
+
+	return true;
+}
+
+void destroyFunctionality(int signal) {
+	destroyModuleManager(&moduleManager);
+	destroyDisplayManager(&displayManager);
+	stashDestroy();
+
+#ifdef REQUIRED_CURL
+	curl_global_cleanup();
+#endif
+
+#ifdef REQUIRED_LIBXML
+	xmlCleanupParser();
+#endif
+}
+
+int main() {
+	struct sigaction signalMgmt;
+	signalMgmt.sa_handler = destroyFunctionality;
+	sigemptyset(&signalMgmt.sa_mask);
+	signalMgmt.sa_flags = 0;
+	sigaction(SIGINT, &signalMgmt, NULL);
+
+	if(!initFunctionality()) {
+		return 1;
+	}
 
 	pause();
 
