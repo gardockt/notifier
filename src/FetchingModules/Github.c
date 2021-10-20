71f759f51e767bdd016180a2f290f504f58a24bf
Tomasz Gardocki <tomasz.gardocki.stud@pw.edu.pl>
2021-10-20 17:47:02 +0200

    Memory and checking-related fixes (2k LoC milestone achieved, by the way)

--- src/FetchingModules/Github.c
+++ src/FetchingModules/Github.c
@@ -8,6 +8,9 @@
 
 #define JSON_STRING(obj, str) json_object_get_string(json_object_object_get((obj),(str)))
 
+#define GITHUB_API_URL "https://api.github.com/notifications"
+#define GITHUB_LAST_UPDATED_DESIRED_LENGTH strlen("1970-01-01T00:00:00Z")
+
 typedef struct {
 	const char* title;
 	const char* repoName;
@@ -22,11 +25,9 @@ int githubCompareDates(const char* a, const char* b) {
 char* githubGenerateNotificationUrl(FetchingModule* fetchingModule, json_object* notification) {
 	GithubConfig* config = fetchingModule->config;
 	NetworkResponse response = {NULL, 0};
+	char* ret = NULL;
 
-	json_object* subject = json_object_object_get(notification, "subject");
-	if(json_object_get_type(subject) != json_type_object) {
-		return NULL;
-	}
+	json_object* subject = json_object_object_get(notification, "subject"); // type checking is done in ParseResponse
 	json_object* commentApiUrlObject = json_object_object_get(subject, "latest_comment_url"); // can be null on PR commits
 	if(json_object_get_type(commentApiUrlObject) != json_type_string) {
 		return NULL;
@@ -36,18 +37,23 @@ char* githubGenerateNotificationUrl(FetchingModule* fetchingModule, json_object*
 	curl_easy_setopt(config->curl, CURLOPT_URL, commentApiUrl);
 	curl_easy_setopt(config->curl, CURLOPT_WRITEDATA, (void*)&response);
 	CURLcode code = curl_easy_perform(config->curl);
+	if(code != CURLE_OK) {
+		return NULL;
+	}
 
 	json_object* commentRoot = json_tokener_parse(response.data);
 	if(json_object_get_type(commentRoot) != json_type_object) {
-		return NULL;
+		goto githubGenerateNotificationUrlFreeAndReturn;
 	}
 	json_object* commentUrl = json_object_object_get(commentRoot, "html_url");
 	if(json_object_get_type(commentUrl) != json_type_string) {
-		return NULL;
+		goto githubGenerateNotificationUrlFreeAndReturn;
 	}
 
-	char* ret = strdup(json_object_get_string(commentUrl));
-	json_object_put(commentUrl);
+	ret = strdup(json_object_get_string(commentUrl));
+githubGenerateNotificationUrlFreeAndReturn:
+	json_object_put(commentRoot);
+	free(response.data);
 	return ret;
 }
 
@@ -123,19 +129,18 @@ void githubParseResponse(FetchingModule* fetchingModule, char* response) {
 
 	for(int i = 0; i < unreadNotifications; i++) {
 		json_object* notification = json_object_array_get_idx(root, i);
-		const char* lastUpdated = JSON_STRING(notification, "updated_at");
-
-		if(lastUpdated == NULL) {
+		json_object* lastUpdatedObject = json_object_object_get(notification, "updated_at");
+		if(json_object_get_type(lastUpdatedObject) != json_type_string) {
 			moduleLog(fetchingModule, 0, "Invalid last update time in notification %d", i);
+			continue;
+		}
+		const char* lastUpdated = json_object_get_string(lastUpdatedObject);
+		if(strlen(lastUpdated) != GITHUB_LAST_UPDATED_DESIRED_LENGTH) {
+			moduleLog(fetchingModule, 0, "Invalid last update time's length in notification %d", i);
 			continue;
 		}
 
 		if(githubCompareDates(lastUpdated, config->lastRead) > 0) {
-			if(newLastRead == NULL || githubCompareDates(lastUpdated, newLastRead) > 0) {
-				free(newLastRead);
-				newLastRead = strdup(lastUpdated);
-			}
-
 			json_object* subject  = json_object_object_get(notification, "subject");
 			if(json_object_get_type(subject) != json_type_object) {
 				moduleLog(fetchingModule, 0, "Invalid subject object in notification %d", i);
@@ -148,10 +153,32 @@ void githubParseResponse(FetchingModule* fetchingModule, char* response) {
 				continue;
 			}
 
-			notificationData.title               = JSON_STRING(subject, "title");
-			notificationData.repoName            = JSON_STRING(repository, "name");
-			notificationData.repoFullName        = JSON_STRING(repository, "full_name");
-			notificationData.url                 = githubGenerateNotificationUrl(fetchingModule, notification);
+			if(newLastRead == NULL || githubCompareDates(lastUpdated, newLastRead) > 0) {
+				free(newLastRead);
+				newLastRead = strdup(lastUpdated);
+			}
+
+			json_object* titleObject = json_object_object_get(subject, "title");
+			json_object* repoNameObject = json_object_object_get(repository, "name");
+			json_object* repoFullNameObject = json_object_object_get(repository, "full_name");
+
+			if(json_object_get_type(titleObject) != json_type_string) {
+				moduleLog(fetchingModule, 0, "Invalid title in notification %d", i);
+				continue;
+			}
+			if(json_object_get_type(repoNameObject) != json_type_string) {
+				moduleLog(fetchingModule, 0, "Invalid repository name in notification %d", i);
+				continue;
+			}
+			if(json_object_get_type(repoFullNameObject) != json_type_string) {
+				moduleLog(fetchingModule, 0, "Invalid full repository name in notification %d", i);
+				continue;
+			}
+
+			notificationData.title         = json_object_get_string(titleObject);
+			notificationData.repoName      = json_object_get_string(repoNameObject);
+			notificationData.repoFullName  = json_object_get_string(repoFullNameObject);
+			notificationData.url           = githubGenerateNotificationUrl(fetchingModule, notification);
 			githubDisplayNotification(fetchingModule, &notificationData);
 			free(notificationData.url);
 		}
@@ -159,6 +186,7 @@ void githubParseResponse(FetchingModule* fetchingModule, char* response) {
 
 	json_object_put(root);
 	if(newLastRead != NULL) {
+		free(config->lastRead);
 		config->lastRead = newLastRead;
 	}
 }
@@ -167,7 +195,7 @@ void githubFetch(FetchingModule* fetchingModule) {
 	GithubConfig* config = fetchingModule->config;
 	NetworkResponse response = {NULL, 0};
 
-	curl_easy_setopt(config->curl, CURLOPT_URL, "https://api.github.com/notifications"); // as GenerateNotificationUrl can change it
+	curl_easy_setopt(config->curl, CURLOPT_URL, GITHUB_API_URL); // as GenerateNotificationUrl can change it
 	curl_easy_setopt(config->curl, CURLOPT_WRITEDATA, (void*)&response);
 	CURLcode code = curl_easy_perform(config->curl);
 
